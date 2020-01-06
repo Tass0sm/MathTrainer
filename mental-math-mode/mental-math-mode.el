a54108c56f3171ae6c0efed4f358b8c862e6ffd2
Anastasios <tassos.manganaris@gmail.com>
2020-01-05 23:22:07 -0500

    Made customizable variables for time display and the operand limit.

--- mental-math-mode/mental-math-mode.el
+++ mental-math-mode/mental-math-mode.el
@@ -1,12 +1,29 @@
+(defgroup mental-math nil
+  ""
+  :tag "Mental Math"
+  :prefix "mental-math-")
+
 (defvar problem-start-time 0)
 
+(defcustom mental-math-display-time t
+  ""
+  :tag "If emacs should display the time taken to solve a problem."
+  :type 'boolean
+  :group 'mental-math)
+
+(defcustom mental-math-operand-limit 10
+  ""
+  :tag "Positive limit for operands in practice questions."
+  :type 'number
+  :group 'mental-math)
+  
 (defun insert-problem ()
   "Insert problem at the end of the buffer."
   (interactive)
   (goto-char (point-max))
   (insert "\n"
-          (number-to-string (random operand-limit)) " + "
-	  (number-to-string (random operand-limit)) " = ")
+          (number-to-string (random mental-math-operand-limit)) " + "
+	  (number-to-string (random mental-math-operand-limit)) " = ")
   (setq problem-start-time (float-time)))
 
 (defun validate--problem ()
@@ -22,28 +39,25 @@
     (save-excursion
       (search-backward "=")
       (forward-char 2)
-      (setq answer (string-to-number (buffer-substring (point-marker) (point-at-eol)))))
+      (setq
+      answer (string-to-number (buffer-substring (point-marker) (point-at-eol)))))
     (setq result (string-to-number (calc-eval problem)))
     (if (eq result answer)
-	(insert "\nCORRECT - ")
-      (insert "\nINCORRECT - "))
-    (insert (format "%.3f s" (- (float-time) start-time)))))
+	(insert "\nCORRECT")
+      (insert "\nINCORRECT"))
+    (if mental-math-display-time
+	(insert (format " - %.3f s" (- (float-time) start-time))))))
 		    
-
-(insert (format "%s" (float-time)))1577922622.25099661577922450.0298865
-
 (defun complete-problem ()
   "Check current line's problem and proceed to the next."
   (interactive)
   (validate--problem)
   (insert-problem))
 
-(defvar operand-limit 10)
-
 (defun set-operand-limit (limit)
   "Set maximum value for problem operands"
   (interactive "nPositive operand limit: ")
-  (setq operand-limit limit))
+  (setq mental-math-operand-limit limit))
 
 (defun mental--math-mode-setup ()
   "Function to set up minimal mode."
